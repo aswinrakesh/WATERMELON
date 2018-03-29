0bc90a7d23a278a14f09574e77011b986b6c394f
aswinrakesh <aswinrakesh1999@gmail.com>
2018-03-29 14:15:47 +0530

    Third Commit

--- app/src/main/java/com/example/hp/jsonparsing/SplashActivity.java
+++ app/src/main/java/com/example/hp/jsonparsing/SplashActivity.java
@@ -1,15 +1,21 @@
 package com.example.hp.jsonparsing;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        String fontpath="fonts/Quikhand.ttf";
+        TextView txt=findViewById(R.id.Quikhand_ttf);
+        Typeface tf=Typeface.createFromAsset(getAssets(),fontpath);
+        txt.setTypeface(tf);
         int SPLASH_TIMEOUT = 4000;
         new Handler().postDelayed(new Runnable() {
             @Override
